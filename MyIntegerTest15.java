79793d614596b44454cc8a2db1c4cd43c5b5d96c
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2020-12-04 15:51:03 +0200

    Create MyIntegerTest15.java

---
+++ MyIntegerTest15.java
@@ -0,0 +1,12 @@
+package oopThinking;
+
+public class MyIntegerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyInteger i = new MyInteger(34);
+		System.out.print(i.getValue());
+		System.out.println(i.equals(17));
+	}
+
+}
